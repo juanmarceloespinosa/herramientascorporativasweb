07283508a9d72a3f18beaf397bf390e24a15fcb6
David Camacho J <dcjpriv@gmail.com>
2024-04-04 10:42:15 -0400

    Updated deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+ENV_FILE=$1
+SCP_NAME=$2
+
 npm run version-inc
-sed -i "s|^\\(PUBLIC_VERSION=\\).*|\1$(jq '.version' -r package.json)|" .env.production
-rm herramientas.zip
-zip -r herramientas.zip . -x node_modules/**\* build/**\*
-scp herramientas.zip anda-herramientas@oracle.anda.fun:/home/anda-herramientas/
+sed -i "s|^\\(PUBLIC_VERSION=\\).*|\1$(jq '.version' -r package.json)|" $ENV_FILE
+rm ${SCP_NAME}.zip
+zip -r ${SCP_NAME}.zip . -x node_modules/**\* build/**\*
+scp ${SCP_NAME}.zip anda-$SCP_NAME@oracle.anda.fun:/home/anda-$SCP_NAME/
 rm -f sed*
-rm -f .env.productione
+rm -f ${ENV_FILE}e
